ff68eaaffbbad4d85d0ebc74c6c0ed9e7ce75695
Coleman Jackson <cole_jackson@ymail.com>
2016-03-28 21:42:16 -0500

    Testing Again

--- app/src/main/java/com/example/coleman/hcigroupproject/Driver.java
+++ app/src/main/java/com/example/coleman/hcigroupproject/Driver.java
@@ -9,6 +9,5 @@ public class Driver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
-        R.id.class;
     }
 }
