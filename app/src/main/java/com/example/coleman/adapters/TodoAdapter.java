8d690c4127d2b47cc6998ee1853fd8ffa82da8fc
sarah <lynch.se22@gmail.com>
2016-04-22 22:48:24 -0500

    Hello

--- app/src/main/java/com/example/coleman/adapters/TodoAdapter.java
+++ app/src/main/java/com/example/coleman/adapters/TodoAdapter.java
@@ -5,12 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.EditText;
 
 import com.example.coleman.app_code.Todo;
 import com.example.coleman.hcigroupproject.R;
@@ -27,6 +29,9 @@ public class TodoAdapter extends ArrayAdapter<Todo>
 {
     private final Activity context;
     private final Todo[] events;
+
+    Boolean expanded = false;
+
 
     public TodoAdapter(Activity context, Todo[] events)
     {
@@ -51,19 +56,61 @@ public class TodoAdapter extends ArrayAdapter<Todo>
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.todo_item, null, true);
 
-        LinearLayout hiddenrow = (LinearLayout) context.findViewById(R.id.hiddenrow);
-        TextView notes = (TextView) context.findViewById(R.id.notes);
-        TextView name = (TextView) context.findViewById(R.id.name);
-        DatePicker date = (DatePicker) context.findViewById(R.id.date);
-        Spinner category = (Spinner) context.findViewById(R.id.category);
-        Switch usedate = (Switch) context.findViewById(R.id.usedate);
+        final LinearLayout hiddenrow = (LinearLayout) context.findViewById(R.id.hiddenrow);
+        final TextView notes = (TextView) context.findViewById(R.id.notes);
+        final EditText name = (EditText) context.findViewById(R.id.name);
+//        DatePicker date = (DatePicker) context.findViewById(R.id.date);
+        final Button dateButton = (Button) context.findViewById(R.id.datebutton);
+        final Spinner category = (Spinner) context.findViewById(R.id.category);
+        final Switch usedate = (Switch) context.findViewById(R.id.usedate);
+        final ImageButton expand = (ImageButton) context.findViewById(R.id.expand);
+
 
         hiddenrow.setVisibility(View.INVISIBLE);
         notes.setVisibility(View.INVISIBLE);
 
+
+
         notes.setText(events[position].getNote());
         name.setText(events[position].getName());
-        date.updateDate(d.getYear(), d.getMonth(), d.getDay());
+//        date.updateDate(d.getYear(), d.getMonth(), d.getDay());
+//        dateButton.set
+
+
+        expand.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (expanded) {
+                    expanded = false;
+                    //reset button image (pointing up)
+
+                    expand.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_24dp);
+                    //set name and date to UNEDITABLE
+                    name.setEnabled(false);
+                    dateButton.setEnabled(false);
+                    dateButton.setClickable(false);
+
+                } else {
+
+                    hiddenrow.setVisibility(View.VISIBLE);
+                    notes.setVisibility(View.VISIBLE);
+                    //reset button image (pointing down)
+                    expand.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_24dp);
+                    //set name and date to EDITABLE
+                    name.setEnabled(true);
+                    dateButton.setEnabled(true);
+                    dateButton.setClickable(true);
+
+                }
+            }
+        });
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View V) {
+                //DatePicker datePicker = new DatePicker();
+
+            }
+        });
+
 
         return rowView;
     }
